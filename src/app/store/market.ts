6687a28f3c8fc2e1cbb40e0b83aec259e8f25726
Shubhaseesh Kumar <s.kumar4@zensar.com>
2022-05-04 23:01:46 +0530

    first commit

---
+++ src/app/store/market.ts
@@ -0,0 +1,35 @@
+import { Product } from "./product.model";
+
+export const PRODUCTS: Product[] = [
+    {
+      id: 0,
+      name: "HP Inspirion",
+      price: 7000
+    },
+    {
+      id: 1,
+      name: "MacBook Pro",
+      price: 15000
+    },
+    {
+      id: 2,
+      name: "Dell 5500",
+      price: 3000
+    },
+    {
+      id: 3,
+      name: "MSI 360",
+      price: 8900
+    },
+    {
+      id: 4,
+      name: "Asus Yogabook",
+      price: 8900
+    },
+    {
+      id: 5,
+      name: "Acer fire",
+      price: 8900
+    },
+  
+]
